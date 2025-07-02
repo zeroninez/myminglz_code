800827502cefdf0d7cb3df957b94f213c0a32b55
Turbobot <turbobot@vercel.com>
2025-07-02 16:11:36 +0900

    feat(create-turbo): apply official-starter transform

--- monorepo/packages/ui/src/SafeArea.tsx
+++
@@ -1,9 +0,0 @@
-export const SafeArea = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <main className="flex flex-col items-center justify-between min-h-dvh p-24">
-      <div className="z-10 items-center justify-between w-full max-w-5xl font-mono text-sm checking lg:flex">
-        {children}
-      </div>
-    </main>
-  );
-};
