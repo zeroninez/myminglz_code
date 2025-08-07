7107070938744a91241613997b5853d5794fbd3e
zeroninez <zeroninez@gmail.com>
2025-08-07 15:08:36 +0900

    feat: update08-07 v0.5

--- mono/apps/generator/app/[locationSlug]/page.tsx
+++ mono/apps/generator/app/[locationSlug]/page.tsx
@@ -277,11 +277,11 @@ export default function LocationGeneratorPage() {
           
           {/* QR 코드 오버레이 */}
           <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-            <img src={qrUrl} alt="QR Code" className="w-[130px] h-[130px]" />
+            <img src={qrUrl} alt="QR Code" className="w-[115px] h-[115px]" />
           </div>
           
           {/* 쿠폰 코드 오버레이 */}
-          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 translate-y-16">
+          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 translate-y-14">
             <button 
               onClick={() => copyToClipboard(code)}
               className="text-[12px] font-mono font-bold flex items-center gap-1 py-2"
